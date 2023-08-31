6d1816ecf0c77cd3362bde2f18e504e26efb46fe
eysaveleva <110624031+eysaveleva@users.noreply.github.com>
2023-08-31 22:07:42 +0300

    Add files via upload

---
+++ scr/components/ImagePopup.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ImagePopup({card, isOpen, onClose}) {
+  return(
+    <div className={`popup popup_type_open-image ${isOpen ? 'popup_opened' : false}`} onClick={onClose} >
+      <div className="popup__container-open">
+        <img className="popup__open-photo"
+          src={card.link}
+          alt={`Картинка - ${card.name}`}/>
+        <p className="popup__open-text">{card.name}</p>
+        <button className="popup__close-btn" type="button" aria-label="Закрыть" onClick={onClose}  />
+      </div>
+    </div>
+  );
+}
+
